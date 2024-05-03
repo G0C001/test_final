c9a275753b87996caf880cdaf3bd1bd793cff548
G0C001 <gokulvbe1303@gmail.com>
2024-05-03 19:59:32 +0530

    gg

--- Test/settings.py
+++ Test/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # STATICFILES_DIRS = [BASE_DIR / "static",]
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =  os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
