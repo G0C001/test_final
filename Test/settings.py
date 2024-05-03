7c0ae51efc3eb17b00d8561589fa1141f69f143f
G0C001 <gokulvbe1303@gmail.com>
2024-05-03 17:36:04 +0530

    final

--- Test/settings.py
+++ Test/settings.py
@@ -27,9 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_application',
-    'crispy_forms',
-    'crispy_bootstrap5',
+    'myapp',
 ]
 
 MIDDLEWARE = [
