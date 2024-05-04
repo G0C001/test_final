fb1a600e5feeff0ceadd38ec274b5afc1b0a582e
G0C001 <gokulvbe1303@gmail.com>
2024-05-04 15:27:52 +0530

    yy

--- Test/urls.py
+++ Test/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('',views.main_page, name="main_page"),
     path('score_card',views.score_card, name="score_card")
     ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
